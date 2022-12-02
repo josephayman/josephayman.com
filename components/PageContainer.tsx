a53a67525c4198c2456d5bf78ae49508c4c83350
Joseph Ayman <josephayman@mail.com>
2022-12-02 21:37:06 +0100

    Page meta fix

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -6,7 +6,7 @@ import Nextlink from "next/link"
 const meta = {
     title: "Next.js + TypeScript Example",
     description: "TypeScript example for Next.js",
-    cardImageURL: "https://og-image.now.sh/Next.js%20Example.png?theme=light&md=1&fontSize=125px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg",
+    imageUrl: "https://og-image.now.sh/Next.js%20Example.png?theme=light&md=1&fontSize=125px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg",
     url: "https://nextjs-typescript-example.vercel.app/",
 }
 
